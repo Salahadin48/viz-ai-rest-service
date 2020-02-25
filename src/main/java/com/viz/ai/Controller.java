26b2062f955c2f3d57a431c4f1ae175b1159521c
Salahadin48 <saladin@Salahadins-MacBook-Pro.local>
2020-02-24 19:49:58 -0600

    make changes to Controller code

--- src/main/java/com/viz/ai/Controller.java
+++ src/main/java/com/viz/ai/Controller.java
@@ -5,6 +5,7 @@ import com.viz.ai.model.HeaderLinkData;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -16,7 +17,11 @@ public class Controller {
         HeaderLinkData headerLinkDataC = new HeaderLinkData("ToolTip", "Twitter");
         HeaderLinkData headerLinkDataD = new HeaderLinkData("Input", "Instagram");
 
-        List<HeaderLinkData> linkDataList = List.of(headerLinkDataA, headerLinkDataB, headerLinkDataC, headerLinkDataD);
+        List<HeaderLinkData> linkDataList = new ArrayList<>();
+        linkDataList.add(headerLinkDataA);
+        linkDataList.add(headerLinkDataB);
+        linkDataList.add(headerLinkDataC);
+        linkDataList.add(headerLinkDataD);
 
         return new HeaderData("Visual AI", linkDataList);
     }
